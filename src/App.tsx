3828889fb49f28dce46a57965eab20827837005e
junaidahmed-One <junaidahmed882000@gmail.com>
2026-09-04 19:52:57 +0530

    add form to enter habit and select freq

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Box, Container, Typography } from "@mui/material";
 import "./App.css";
+import AddHabitForm from "./components/add-habit-form";
 
 function App() {
-  return <div></div>;
+  return (
+    <Container>
+      <Box>
+        <Typography variant="h2" component="h1" gutterBottom align="center">
+          Habit Tracker
+        </Typography>
+      </Box>
+      <AddHabitForm />
+    </Container>
+  );
 }
 
 export default App;
